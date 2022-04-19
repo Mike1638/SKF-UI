9b42971bb4eacb1c1688e2991419e4e9f859966a
hisoka1638 <1399952343@qq.com>
2022-04-19 23:26:13 +0800

    Switch增加点击动效 优化合并代码

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,51 +1,63 @@
 <template>
   <div>
-    <button :class="{checked:value}" @click="toggle">
-        <span></span>
+    <button :class="{ checked: value }" @click="toggle">
+      <span></span>
     </button>
   </div>
 </template>
 
 <script lang="ts">
 export default {
-    props:{value:Boolean},
-    setup(props,context){
-        const toggle = ()=>{
-           context.emit('update:value',!props.value)
-        }
-        return {toggle}
-    }
+  props: { value: Boolean },
+  setup(props, context) {
+    const toggle = () => {
+      context.emit("update:value", !props.value);
+    };
+    return { toggle };
+  },
 };
 </script>
 
 
 <style lang="scss" scoped>
-$bh:22px;
-$rh:$bh - 4px;
-button{
-    width: $bh*2;
-    height: $bh;
-    border: none;
-    border-radius: $bh;
-    position: relative;
-     background: rgb(191, 191, 191);   
-}
-span{
+$bh: 22px;
+$rh: $bh - 4px;
+button {
+  width: $bh * 2;
+  height: $bh;
+  border: none;
+  border-radius: $bh;
+  position: relative;
+  background: rgb(191, 191, 191);
+  > span {
     position: absolute;
-    top:2px;
-    left:2px; 
+    top: 2px;
+    left: 2px;
     background: rgb(245, 245, 247);
     width: $rh;
     height: $rh;
-    border-radius:$rh/2;
-    transition: left 0.25s;
-}
-button.checked{
-    background: rgb(24, 144, 255);   
-    span{
-      left:calc(100% - #{$rh} - 2px); 
+    border-radius: $rh/2;
+    transition: all 250ms;
+  }
+  &:focus {
+    outline: none;
+  }
+  &.checked {
+    background: rgb(24, 144, 255);
+    span {
+      left: calc(100% - #{$rh} - 2px);
     }
+  }
+  &:active {
+    span {
+      width: $rh + 4px;
+    }
+  }
+  &.checked:active {
+    span {
+      width: $rh + 4px;
+      margin-left: -4px;
+    }
+  }
 }
-
-
 </style>
