c2a12e06f58fa3745a1cdf1abaa15681fe4dd76c
hisoka1638 <1399952343@qq.com>
2022-04-23 16:29:49 +0800

    完成 size level disabled

--- src/components/Buttondemo.vue
+++ src/components/Buttondemo.vue
@@ -6,6 +6,42 @@
     <Button theme="button">你好</Button>
     <Button theme="link">你好</Button>
     <Button theme="text">你好</Button>
+    <h1>示例2</h1>
+    <div>
+      <Button size="big">大大大</Button>
+      <Button>普通</Button>
+      <Button size="small">小小小</Button>
+    </div>
+    <div>
+      <Button theme="link" size="big">大大大</Button>
+      <Button theme="link">普通</Button>
+      <Button theme="link" size="small">小小小</Button>
+    </div>
+    <div>
+      <Button theme="text" size="big">大大大</Button>
+      <Button theme="text">普通</Button>
+      <Button theme="text" size="small">小小小</Button>
+    </div>
+    <h1>示例3</h1>
+    <div>
+      <Button level="main">主要按钮</Button>
+      <Button>普通</Button>
+      <Button level="danger">危险按钮</Button>
+    </div>
+    <div>
+      <Button theme="link" level="main">主要按钮</Button>
+      <Button theme="link">普通</Button>
+      <Button theme="link" level="danger">危险按钮</Button>
+    </div>
+    <div>
+      <Button theme="text" level="main">主要按钮</Button>
+      <Button theme="text">普通</Button>
+      <Button theme="text" level="danger">危险按钮</Button>
+    </div>
+    <h1>示例4</h1>
+    <Button disabled>不可用</Button>
+    <Button theme="link" disabled>不可用</Button>
+    <Button theme="text" disabled>不可用</Button>
   </div>
 </template>
 
@@ -13,7 +49,6 @@
 import Button from "../lib/Button.vue";
 export default {
   components: { Button },
-  
 };
 </script>
 
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,55 +1,161 @@
 <template>
-  <button class="skf-button" :class="`skf-theme-${theme}`">
+  <button class="skf-button" :class="classes" :disabled="disabled">
     <slot />
   </button>
 </template>
 
 <script lang="ts">
+import { computed } from "vue";
+
 export default {
   props: {
     theme: {
       type: String,
       default: "button",
     },
+    size: {
+      type: String,
+      default: "normal",
+    },
+    level: {
+      type: String,
+      default: "normal",
+    },
+    disabled:{
+      type:Boolean,
+      default:false
+    }
+  },
+  setup(props) {
+    const { theme, size, level } = props;
+    const classes = computed(() => {
+      return {
+        [`skf-theme-${theme}`]: theme,
+        [`skf-size-${size}`]: size,
+        [`skf-level-${level}`]: level,
+      };
+    });
+    return { classes };
   },
 };
 </script>
 
 <style lang="scss">
 .skf-button {
-    box-sizing: border-box;
-    border:none;
-    border:1px solid rgb(217, 217, 217);
-    height: 32px;
-    border-radius: 6px;
-    padding: 0 12px;
-    cursor: pointer;
-    display:inline-flex;
-    justify-content: center;
-    align-items:center;
-    color:rgb(38, 38, 38); 
-    background: white;
-    box-shadow: 0 1px 0 fade-out(black, 0.95);
-    & + & {
-     margin-left: 8px;
-    }
-    &:hover,
-    &:focus{
-      color: rgb(64, 169, 255);
-      border-color:rgb(64, 169, 255) ;
-    }
-    &:focus{
-      outline: none;
-    }
-    &::-moz-focus-inner{
-      border: 0;
-    }
-
-  &.skf-theme-button {
+  box-sizing: border-box;
+  border: none;
+  border: 1px solid rgb(217, 217, 217);
+  height: 32px;
+  border-radius: 6px;
+  padding: 0 12px;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  color: rgb(38, 38, 38);
+  background: white;
+  box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background  250ms;
+  & + & {
+    margin-left: 8px;
+  }
+  &:hover,
+  &:focus {
+    color: rgb(64, 169, 255);
+    border-color: rgb(64, 169, 255);
+  }
+  &:focus {
+    outline: none;
+  }
+  &::-moz-focus-inner {
+    border: 0;
   }
   &.skf-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: rgb(24, 144, 255);
+    &:hover,
+    &:focus {
+      color: rgb(64, 169, 255);
+    }
   }
   &.skf-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover {
+      background: rgb(250, 250, 250);
+    }
+  }
+  &.skf-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+  &.skf-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
+  &.skf-theme-button {
+    &.skf-level-main {
+      background: rgb(64, 169, 255);
+      color: white;
+      border-color:rgb(64, 169, 255);
+      &:hover,&:focus{
+      background: darken(rgb(64,169,255),10%);
+      border-color: darken(rgb(64,169,255),10%);
+      }
+    }
+    &.skf-level-danger {
+      background: rgb(255, 77, 79);
+      color: white;
+      border-color: rgb(255, 77, 79);
+      &:hover,&:focus{
+      background: rgb(255, 120, 117) ;
+      border-color: rgb(255, 120, 117); 
+      }
+    }
+    &[disabled]{
+      cursor: not-allowed;
+      color:rgb(191, 191, 191);
+      &:hover{
+      border-color:rgb(217, 217, 217);
+      }
+    }
+  }
+  &.skf-theme-link {
+    &.skf-level-danger {
+      color: rgb(255, 77, 79);
+       &:hover,&:focus{
+      color: rgb(255, 120, 117); 
+      }
+    }
+    &[disabled]{
+      cursor: not-allowed;
+      color:rgb(191, 191, 191)
+    }
+  }
+  &.skf-theme-text{
+    &.skf-level-main{
+   color: rgb(64, 169, 255);
+   &:hover,&:focus{
+     color: darken(rgb(64,169,255),10%);
+   }
+    }
+    &.skf-level-danger {
+      color: rgb(255, 77, 79);
+       &:hover,&:focus{
+      color: rgb(255, 120, 117); 
+      }
+    }
+     &[disabled]{
+      cursor: not-allowed;
+      color:rgb(191, 191, 191)
+    }
   }
 }
 </style>
+
+
+
